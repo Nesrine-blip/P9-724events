8a63b78faeff43544ee1e8a88421bc14ee3dfc94
Nesrine-blip <salahnesrine29@gmail.com>
2025-10-20 14:00:46 -0700

    fix: show confirmation popup after form submission

--- src/containers/Form/index.js
+++ src/containers/Form/index.js
@@ -16,6 +16,7 @@ const Form = ({ onSuccess, onError }) => {
       try {
         await mockContactApi();
         setSending(false);
+        onSuccess(); // LIGNE AJOUTÉE - Appel de la fonction onSuccess
       } catch (err) {
         setSending(false);
         onError(err);
